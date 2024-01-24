b0f7d906ba243d889d4777fbd2e3730f3401c88e
binaysahoo <binay.sahoo1982@gmail.com>
2024-01-24 17:49:59 +0530

    Update message_restapi.pl

--- perl/message_restapi.pl
+++ perl/message_restapi.pl
@@ -12,3 +12,26 @@ my $request = HTTP::Request->new(
 );
 
 $ua->request($request);
+
+
+
+use LWP::UserAgent;
+use HTTP::Request;
+
+my $rabbitmq_api_url = 'http://guest:guest@localhost:15672/api/exchanges/%2F/my_exchange/publish';
+my $json_message = '{"key": "value"}';
+
+my $ua = LWP::UserAgent->new;
+my $request = HTTP::Request->new(
+   POST => $rabbitmq_api_url,
+   ['Content-Type' => 'application/json'],
+   $json_message
+);
+
+my $response = $ua->request($request);
+
+if ($response->is_success) {
+   print "Message published successfully.\n";
+} else {
+   print "Error publishing message: " . $response->status_line . "\n";
+}
